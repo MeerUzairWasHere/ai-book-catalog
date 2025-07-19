444fda507cd6739d5aae1365938e2e859d16e65f
Meer Uzair <meer.uxair007@gmail.com>
2025-07-20 01:29:52 +0530

    dep

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,29 @@
 require("dotenv").config();
 const connectDB = require("./db");
 const express = require("express");
+import path from "path";
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
 
 const bookRoutes = require("./routes/book.routes");
 
 app.use("/api/v1/books", bookRoutes);
+app.get("/api/v1/health", (req, res) => {
+  res.status(200).json({ msg: "Server is running!" });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+});
+
 
 const start = async () => {
   try {
